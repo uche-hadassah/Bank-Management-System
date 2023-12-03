cb9ad647ee7649dfa3bb4d3ef54a2f2a02d7c22d
Hadassah Uche <uhadassah1@gmail.com>
2023-12-03 21:26:28 +0200

    Let's get started again.

---
+++ UcheHadassah-PROJECT/HadassahUche-PROJECT.cpp
@@ -0,0 +1,32 @@
+/*In this second programming assignment you will implement a simple bank 
+application. Your bank application will support the following features:
+ü Open a new account, i.e. register a new user. The information stored is: first name, 
+last name, address, phone number, and birth date. The account is also assigned a 
+unique account number. 
+ü Close an account. All the information stored about the particular customer is 
+deleted. 
+ü Deposit a given amount to an account. The user has to provide the account 
+number.
+ü Withdraw a given amount from an account. The user has to provide the account 
+number and at no time can the balance of the account be less than zero.
+ü Display account information when given the account number.
+ü Print all the accounts sorted on customer account number, or the amount in the 
+account.
+ü Search and display the accounts based on customer last name, or phone number.
+ü Save all the accounts to a file and also allow to read in from a file
+REQUIREMENTS
+a) Your code has to be:
+- well-structured (indention, etc)
+- well-commented(at the top of the file, before each function, and for
+“important” parts of the code)
+- define variables, constants, etc following the programming style suggested
+by the book
+b) No global variables
+c) Each operation of the application has to be handled by a separate function
+d) All functions have to appear after main
+e) Your program should be able to handle any user input. In case the user input is
+wrong you should print an error message before displaying the menu again.
+f) To store the customer information you can choose to use either a struct or a class.
+To store all the accounts you can choose to use an array or a Vector. For the strings
+you can use either a c-string or the string class. You are also free to use exception
+handling.*/
